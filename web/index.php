9fad7addb3b8a94d9523021dfc9f3a0b53926650
Alexander <xel_s@mail.ru>
2024-01-31 13:11:40 +0300

    корекция путей сервера

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 $pth=realpath(__DIR__ .'/../../').'/yiicore';
 // comment out the following two lines when deployed to production
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require "$pth/vendor/autoload.php";
 require "$pth/vendor/yiisoft/yii2/Yii.php";
