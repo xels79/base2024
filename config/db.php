fe119638f38f37b75c401415b767d17e9e00aba5
Alexander <xel_s@mail.ru>
2024-01-31 13:11:07 +0300

    db изменены параметры базы

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=u0931523_base_asterion',
-    'username' => 'u0931523_b_aster',
-    'password' => '0A9o6I7z',
+    'dsn' => 'mysql:host=localhost;dbname=basetest',
+    'username' => 'basetest',
+    'password' => 'jEr2h!SRlN_bVl0V',
     'charset' => 'utf8',
 ];
