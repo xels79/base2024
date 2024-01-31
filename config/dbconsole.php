ce5818128084f935ce3a8efc4f13635e5cab8dc8
Alexander <xel_s@mail.ru>
2024-01-31 13:13:53 +0300

    параметры базы изменены

--- config/dbconsole.php
+++ config/dbconsole.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=u0931523_base_asterion',
+    // 'username' => 'u0931523_b_aster',
+    // 'password' => '0A9o6I7z',
+    // 'charset' => 'utf8',
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=u0931523_base_asterion',
-    'username' => 'u0931523_b_aster',
-    'password' => '0A9o6I7z',
+    'dsn' => 'mysql:host=localhost;dbname=basetest',
+    'username' => 'basetest',
+    'password' => 'jEr2h!SRlN_bVl0V',
     'charset' => 'utf8',
+
 ];
